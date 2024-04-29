bd8820cc1176bee9733e9e5ffec4e056cfb06fc3
Elise Acher eliseacher729@gmail.com <eliseacher729@gmail.com>
2024-04-29 20:04:45 +0100

    adding validation to game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,5 +7,33 @@ class GamesController < ApplicationController
     @guess = params[:word]
     @letters = params[:letters]
 
+    if valid_word?(@guess) && included?(@letters, @guess.upcase)
+      @answer = "Congratulations!"
+    elsif valid_word?(@guess)
+      @answer = "Not in the letters provided"
+    else
+      @answer = "Sorry, not an English Word "
+    end
   end
+
+
+  private
+
+
+  def valid_word?(attempt)
+    # url = "" => Le Wagon API is currently not working
+    # file = URI.open(url).read
+    # words = JSON.parse(file)
+    # return words["found"]
+    return true
+
+  end
+
+  def included?(letter, attempt)
+    attempt.chars.all? do |letter|
+      attempt.count(letter) <= letter.count(letter)
+    end
+  end
+
+
 end
